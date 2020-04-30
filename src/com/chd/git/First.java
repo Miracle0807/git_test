12be35763e307ffc10a241e226120f0fb9cc0b6f
gaoze <1497369838@qq.com>
2020-04-30 16:05:15 +0800

    把本地工程commit

---
+++ src/com/chd/git/First.java
@@ -0,0 +1,17 @@
+package com.chd.git;
+
+/**
+ * @Author: GaoZe
+ * @Description:
+ * @Date: Create in 01:36 2020/4/30
+ */
+public class First {
+    public static void main(String[] args) {
+        System.out.println("高泽");
+        System.out.println("高泽");
+        System.out.println("高泽");
+        System.out.println("高泽");
+        System.out.println("高泽");
+        System.out.println("高泽");
+    }
+}
